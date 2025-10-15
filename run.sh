65d85eabd97cb9dd7975e629fefb38f34ac5a2b6
extremq <45830561+extremq@users.noreply.github.com>
2025-10-15 22:33:44 +0300

    update run

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build . -t bakaneko
-nohup docker run -d --rm --restart unless-stopped bakaneko > ./log.txt 2> ./err.txt & -d --rm --restart unless-stopped
+nohup docker run -d --rm --restart unless-stopped bakaneko > ./log.txt 2> ./err.txt &
